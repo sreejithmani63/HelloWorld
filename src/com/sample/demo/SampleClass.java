da46da281348a6dcc3b5121dfc54a38e30029c5d
Sreejith Mani <thesreestrikes@gmail.com>
2017-09-13 10:33:27 +0530

    Added SampleClass

---
+++ src/com/sample/demo/SampleClass.java
@@ -0,0 +1,12 @@
+package com.sample.demo;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubs
+		
+		System.out.println("Hello world");
+
+	}
+
+}
